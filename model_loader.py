8ca25b393bc46b6f9c23d920b3fc35c41d23be1f
Shakiba Moradian <moradias@informatik.uni-freiburg.de>
2024-04-30 12:12:09 +0200

    add loader for supernet of nb201 and darts from checkpoint

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,10 @@
 import os
 import cifar10.model_loader
 
-def load(dataset, model_name, model_file, data_parallel=False):
-    if dataset == 'cifar10':
+def load(dataset, model_name, model_file, data_parallel=False, config={}):
+    confopt_models = ["nb201", "darts", "robust_darts"]
+    if model_name in confopt_models:
+        net = cifar10.model_loader.load_confopt_model(model_name, model_file, data_parallel, config)
+    elif dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
     return net
